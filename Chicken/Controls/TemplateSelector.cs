a4a4847f8874bbbafe6a90e0a7e89f4cde04b918
im_wower <george674834080@gmail.com>
2013-06-12 11:39:29 +0800

    finish new message design

--- Chicken/Controls/TemplateSelector.cs
+++ Chicken/Controls/TemplateSelector.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Net;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using Chicken.ViewModel.Home.Base;
 
 namespace Chicken.Controls
@@ -40,7 +32,6 @@ namespace Chicken.Controls
                 return MessageSentByMeTemplate;
             }
             return MessageTemplate;
-            //ListBox.ItemContainerStyleProperty
         }
     }
 }
