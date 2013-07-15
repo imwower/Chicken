43d7660e315b5a72f5721287305f5e8b9c2d0857
im_wower <george674834080@gmail.com>
2013-07-15 17:32:08 +0800

    localize

--- Chicken/Resources/AppResources.Designer.cs
+++ Chicken/Resources/AppResources.Designer.cs
@@ -574,7 +574,7 @@ namespace Chicken.Resources {
         }
         
         /// <summary>
-        ///   查找类似 Chicken for Windows Phone，是一个社交网络客户端。可以用来查看朋友们的状态，发布新的动态等。 的本地化字符串。
+        ///   查找类似 Chicken是一个Windows Phone平台的社交网络客户端。可以用来查看朋友们的状态，发布新的动态等。 的本地化字符串。
         /// </summary>
         public static string SettingsPage_About_Description {
             get {
