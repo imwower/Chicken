bb0a069d0786019166e7732c63727fdffd596403
im_wower <george674834080@gmail.com>
2013-07-16 17:51:20 +0800

    search page auto textbox

--- Chicken/View/SearchPage.xaml.cs
+++ Chicken/View/SearchPage.xaml.cs
@@ -1,12 +1,13 @@
-﻿using System.Windows.Controls;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 using Chicken.ViewModel.Search;
 
 namespace Chicken.View
 {
     public partial class SearchPage : PivotPageBase
     {
+        #region properties
         private SearchViewModel searchViewModel;
+        #endregion
 
         public SearchPage()
         {
@@ -18,13 +19,6 @@ namespace Chicken.View
         }
 
         #region search textbox
-        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            var textbox = (TextBox)sender;
-            var binding = textbox.GetBindingExpression(TextBox.TextProperty);
-            binding.UpdateSource();
-        }
-
         private void SearchBox_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
